618e138490636d82af82d6c30a5c5becf0850298
Julio Cesar Lima <limajuliocesar2010@gmail.com>
2026-06-18 09:09:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const modeButton = document.getElementById("mode-btn");
 var mode = "black";
 
 modeButton.addEventListener("click", function(){
-	body = document.body;
+	const body = document.body;
 	if(mode == "black")
 	{
-		body.style.backgroundColor = "white";
-		body.style.color = "black";
+		body.style.background = "linear-gradient(135deg, #000000, #004e1a)";
+		body.style.color = "white";
 		modeButton.textContent = "Switch to dark mode";
 		mode = "white";
 	}
 	else
 	{
-		body.style.backgroundColor = "black";
 		body.style.background = "linear-gradient(135deg, #5fffb4, #ffffff)";
 		body.style.color = "black";
 		modeButton.textContent = "Switch to light mode";
